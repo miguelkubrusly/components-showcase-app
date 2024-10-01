af918c743fb4a48f5b122e580f9ac7c08334570a
Miguel Kubrusly <125589621+miguelkubrusly@users.noreply.github.com>
2024-10-01 18:56:04 -0300

    feat: add stylized dummy buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
+import { GoBell, GoSortDesc, GoTelescope } from "react-icons/go";
 import Button from "./Button";
 
 function App() {
   return (
     <div>
-      <Button>Click Here</Button>
-      <Button>Super Button</Button>
-      <Button>Subscribe</Button>
-      <Button>I Do Nothing</Button>
-      <Button>Me Too</Button>
+      <Button primary>
+        <GoBell />
+        Click Here
+      </Button>
+      <Button secondary>
+        <GoSortDesc />
+        Super Button
+      </Button>
+      <Button success outline rounded>
+        Subscribe
+      </Button>
+      <Button warning rounded>
+        <GoTelescope />I Do Nothing
+      </Button>
+      <Button danger outline>
+        Me Too
+      </Button>
     </div>
   );
 }
--- src/Button.tsx
+++ src/Button.tsx
@@ -1,3 +1,6 @@
+import { twMerge } from "tailwind-merge";
+import className from "classnames";
+
 function Button({
   primary,
   secondary,
@@ -8,9 +11,26 @@ function Button({
   rounded,
   children,
 }: ButtonProps) {
+  const classes: string = twMerge(
+    className("flex text-center px-3 py-1.5 border m-2", {
+      "border-blue-500 bg-blue-500 text-white": primary,
+      "border-gray-900 bg-gray-900 text-white": secondary,
+      "border-green-500 bg-green-500 text-white": success,
+      "border-yellow-400 bg-yellow-400 text-white": warning,
+      "border-red-500 bg-red-500 text-white": danger,
+      "rounded-full": rounded,
+      "bg-transparent": outline,
+      "text-blue-500": outline && primary,
+      "text-gray-900": outline && secondary,
+      "text-green-500": outline && success,
+      "text-yellow-400": outline && warning,
+      "text-red-500": outline && danger,
+    })
+  );
+
   return (
     <div>
-      <button>{children}</button>
+      <button className={classes}>{children}</button>
     </div>
   );
 }
