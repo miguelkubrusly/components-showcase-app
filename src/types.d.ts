a4abe62b3485169f2b6064f318079e3a9008da2a
Miguel Kubrusly <125589621+miguelkubrusly@users.noreply.github.com>
2024-10-02 17:58:35 -0300

    feat: make type for Accordions' prop items

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,11 +1,23 @@
-
 declare global {
+
 
   type ExcludeFromObject<T extends any[], U> = {
     [K in keyof T]: T[K] extends U ? never : T[K]
   }[number]
 
   type Exclusive<T extends PropertyKey[], U = any> = T[number] extends infer E ? E extends string ? Record<E, U> & { [k in ExcludeFromObject<T, E>]?: never } : never : never
+
+  type MapObject<T, O extends object> = {
+    [K in keyof O]: T
+  }
+
+  interface AccordionItem {
+    id: number;
+    label: string;
+    content: string
+  }
+
+  type AccordionItemsProp = MapObject<AccordionItem, items>
 
   type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> &
     Partial<{
