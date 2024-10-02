fb7858b2a6ca250ecb62df89235b3c4aac6a5864
Miguel Kubrusly <125589621+miguelkubrusly@users.noreply.github.com>
2024-10-02 19:55:26 -0300

    feat: make item close with second click

--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -2,21 +2,32 @@ import { useState } from "react";
 import { GoChevronDown, GoChevronLeft } from "react-icons/go";
 
 function Accordion({ items }: AccordionItemsProp) {
-  const [expandedIndex, setExpandedIndex] = useState(-1);
+  const [expandedList, setExpandedList] = useState([-1]);
+  let lastIndex = expandedList[expandedList.length - 1];
 
-  const handleClick = (newIndex: number) => setExpandedIndex(newIndex);
+  const handleClick = (newIndex: number) => {
+    const newList = lastIndex === newIndex ? [-1] : [newIndex];
+    setExpandedList(newList);
+  };
 
   const renderedItems = items.map((item: any, index: number) => {
-    const isExpanded = index === expandedIndex;
+    const isExpanded = index === lastIndex;
     const icon: JSX.Element = (
-      <span>{isExpanded ? <GoChevronDown /> : <GoChevronLeft />}</span>
+      <span className="text-xl">
+        {isExpanded ? <GoChevronDown /> : <GoChevronLeft />}
+      </span>
     );
 
     return (
-      <div key={item.id}>
-        {icon}
-        <div onClick={() => handleClick(index)}>{item.label}</div>
-        {isExpanded && <div>{item.content}</div>}
+      <div key={item.id} className="w-1/5">
+        <div
+          className="flex p-3 bg-gray-50 border-b items-center cursor-pointer justify-between"
+          onClick={() => handleClick(index)}
+        >
+          {item.label}
+          {icon}
+        </div>
+        {isExpanded && <div className="border-b p-5">{item.content}</div>}
       </div>
     );
   });
