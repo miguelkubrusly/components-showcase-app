c0960d7575807d71bfba6e0b0470794accf7e641
Miguel Kubrusly <125589621+miguelkubrusly@users.noreply.github.com>
2024-10-02 18:51:54 -0300

    feat: add icon to accordion
    -moved event handler out of the map function

--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -1,14 +1,21 @@
 import { useState } from "react";
+import { GoChevronDown, GoChevronLeft } from "react-icons/go";
 
 function Accordion({ items }: AccordionItemsProp) {
   const [expandedIndex, setExpandedIndex] = useState(-1);
 
+  const handleClick = (newIndex: number) => setExpandedIndex(newIndex);
+
   const renderedItems = items.map((item: any, index: number) => {
     const isExpanded = index === expandedIndex;
+    const icon: JSX.Element = (
+      <span>{isExpanded ? <GoChevronDown /> : <GoChevronLeft />}</span>
+    );
 
     return (
       <div key={item.id}>
-        <div onClick={() => setExpandedIndex(index)}>{item.label}</div>
+        {icon}
+        <div onClick={() => handleClick(index)}>{item.label}</div>
         {isExpanded && <div>{item.content}</div>}
       </div>
     );
