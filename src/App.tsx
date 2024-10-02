a101e74d308a07d6384103ef46f6ba291ba686f2
Miguel Kubrusly <125589621+miguelkubrusly@users.noreply.github.com>
2024-10-02 17:58:54 -0300

    test: display Accordion logic on screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,32 @@
+import Accordion from "./components/Accordion";
+
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <Accordion
+        items={[
+          {
+            id: Math.floor(Math.random() * 8),
+            label: "This is a Accordion Label",
+            content:
+              "And this is a very repetitive content. And this is a very repetitive content. And this is a very repetitive content. And this is a very repetitive content. ",
+          },
+          {
+            id: Math.floor(Math.random() * 8),
+            label: "This is a Accordion Label",
+            content:
+              "And this is a very repetitive content. And this is a very repetitive content. And this is a very repetitive content. And this is a very repetitive content. ",
+          },
+          {
+            id: Math.floor(Math.random() * 8),
+            label: "This is a Accordion Label",
+            content:
+              "And this is a very repetitive content. And this is a very repetitive content. And this is a very repetitive content. And this is a very repetitive content. ",
+          },
+        ]}
+      />
+    </div>
+  );
 }
 
 export default App;
