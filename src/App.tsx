f39151794eac93e00d5193bbaefcc918806fdd27
Miguel Kubrusly <125589621+miguelkubrusly@users.noreply.github.com>
2024-10-02 20:15:28 -0300

    fix: use correct operator for exponentiation

--- src/App.tsx
+++ src/App.tsx
@@ -6,19 +6,19 @@ function App() {
       <Accordion
         items={[
           {
-            id: Math.floor(Math.random() * 10 ^ 9),
+            id: Math.floor(Math.random() * 10 ** 9),
             label: "This is an Accordion Label",
             content:
               "And this is a very repetitive content. And this is a very repetitive content. And this is a very repetitive content. And this is a very repetitive content. ",
           },
           {
-            id: Math.floor(Math.random() * 10 ^ 9),
+            id: Math.floor(Math.random() * 10 ** 9),
             label: "This is an Accordion Label",
             content:
               "And this is a very repetitive content. And this is a very repetitive content. And this is a very repetitive content. And this is a very repetitive content. ",
           },
           {
-            id: Math.floor(Math.random() * 10 ^ 9),
+            id: Math.floor(Math.random() * 10 ** 9),
             label: "This is an Accordion Label",
             content:
               "And this is a very repetitive content. And this is a very repetitive content. And this is a very repetitive content. And this is a very repetitive content. ",
