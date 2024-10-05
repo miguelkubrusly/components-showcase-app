8bf7624535c2b415f59ef6ffb0fbb050a399e05e
Miguel Kubrusly <125589621+miguelkubrusly@users.noreply.github.com>
2024-10-05 20:29:53 -0300

    style(global): remove unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
-import { useContext } from "react";
-import NavigationContext from "./context/navigation";
-import ButtonPage from "./pages/ButtonPage";
 import DropdownPage from "./pages/DropdownPage";
 import AccordionPage from "./pages/AccordionPage";
 import Link from "./components/Link";
 import Router from "./components/Router";
 
 function App() {
-  // const { currentPath }: any = useContext(NavigationContext);
-
   return (
     <div>
       <Link to={"./accordion"}>Go to accordion</Link>
