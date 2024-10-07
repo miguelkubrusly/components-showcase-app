ccd1b5f6fad51c365914d4bba7fffa3b59119fa9
Miguel Kubrusly <125589621+miguelkubrusly@users.noreply.github.com>
2024-10-07 15:47:32 -0300

    feat(modal): buggy personal attempt
    -can't make it work completely
    -adding useEffect is breaking modal in ways I don't understand

--- src/pages/ModalPage.tsx
+++ src/pages/ModalPage.tsx
@@ -1,20 +1,50 @@
 import Modal from "../components/Modal";
 import Button from "../components/Button";
-import { useState } from "react";
+import { RefObject, useEffect, useRef, useState } from "react";
 
 function ModalPage() {
   const [isOpen, setIsOpen] = useState(false);
 
-  const handleClick = () => {
+  const modalEl: RefObject<HTMLDivElement> = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const closingHandler = (event: Event) => {
+      if (!modalEl.current) {
+        return;
+      }
+      if (!modalEl.current.contains(event.target as Node | null)) {
+        setIsOpen(false);
+      }
+    };
+    if (isOpen) {
+      document.addEventListener("click", closingHandler);
+    } else {
+      return () => {
+        document.removeEventListener("click", closingHandler);
+      };
+    }
+
+    return () => {
+      document.removeEventListener("click", closingHandler);
+    };
+  }, [isOpen]);
+
+  const handleOpen = () => {
+    console.log("clicked");
+    console.log(isOpen);
     setIsOpen(true);
   };
 
   return (
     <div>
-      <Button primary onClick={handleClick}>
+      <Button primary onClick={handleOpen}>
         Open Modal
       </Button>
-      {isOpen && <Modal />}
+      {isOpen && (
+        <div ref={modalEl}>
+          <Modal />
+        </div>
+      )}
     </div>
   );
 }
