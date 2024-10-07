9e5307681ed533f40681cd30702fa2a52886d268
Miguel Kubrusly <125589621+miguelkubrusly@users.noreply.github.com>
2024-10-07 16:27:03 -0300

    fix(modal): simpler logic to close it on bg click
    -hinted by teacher
    -just make an onClose prop to give to modal--
    --and add it to the grayed background div

--- src/pages/ModalPage.tsx
+++ src/pages/ModalPage.tsx
@@ -1,50 +1,22 @@
 import Modal from "../components/Modal";
 import Button from "../components/Button";
-import { RefObject, useEffect, useRef, useState } from "react";
+import { useState } from "react";
 
 function ModalPage() {
   const [isOpen, setIsOpen] = useState(false);
 
-  const modalEl: RefObject<HTMLDivElement> = useRef<HTMLDivElement>(null);
+  const handleClose = () => setIsOpen(false);
 
-  useEffect(() => {
-    const closingHandler = (event: Event) => {
-      if (!modalEl.current) {
-        return;
-      }
-      if (!modalEl.current.contains(event.target as Node | null)) {
-        setIsOpen(false);
-      }
-    };
-    if (isOpen) {
-      document.addEventListener("click", closingHandler);
-    } else {
-      return () => {
-        document.removeEventListener("click", closingHandler);
-      };
-    }
-
-    return () => {
-      document.removeEventListener("click", closingHandler);
-    };
-  }, [isOpen]);
-
-  const handleOpen = () => {
-    console.log("clicked");
-    console.log(isOpen);
+  const handleClick = () => {
     setIsOpen(true);
   };
 
   return (
     <div>
-      <Button primary onClick={handleOpen}>
+      <Button primary onClick={handleClick}>
         Open Modal
       </Button>
-      {isOpen && (
-        <div ref={modalEl}>
-          <Modal />
-        </div>
-      )}
+      {isOpen && <Modal onClose={handleClose} />}
     </div>
   );
 }
