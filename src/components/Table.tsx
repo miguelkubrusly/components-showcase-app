1c65dd3e43cb78b49a4f917acae317c584cc368a
Miguel Kubrusly <125589621+miguelkubrusly@users.noreply.github.com>
2024-10-07 19:06:36 -0300

    feat(table): add keys to list elements

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,14 +1,14 @@
 function Table({ data }: TableProp) {
   const renderedData = data.map((item: TableItem) => {
     const renderedItem = Object.values(item).map((value) => {
-      return <td>{value}</td>;
+      return <td key={value}>{value}</td>;
     });
-    return <tr>{renderedItem}</tr>;
+    return <tr key={item.name}>{renderedItem}</tr>;
   });
 
   const keys = Object.keys(data[0]);
   const renderedKeys = keys.map((key) => {
-    return <th>{key}</th>;
+    return <th key={key}>{key}</th>;
   });
 
   return (
