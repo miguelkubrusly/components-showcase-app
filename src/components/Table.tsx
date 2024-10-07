4b5b81398adf6cdc3ae7c530ee477b94e7c41132
Miguel Kubrusly <125589621+miguelkubrusly@users.noreply.github.com>
2024-10-07 19:50:59 -0300

    feat(table): add personal styling

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -8,15 +8,18 @@ function Table({ data }: TableProp) {
   const renderedData = data.map((item: TableItem, index) => {
     const renderedItem = Object.entries(item).map(([key, value]) => {
       return key === "color" ? (
-        <div className={renderedColors[index]} key={value}></div>
+        <div
+          className={`w-8 h-8 ${renderedColors[index]} mx-auto`}
+          key={value}
+        ></div>
       ) : (
-        <td className="p-3" key={value}>
+        <td className="py-6 px-3 text-center align-middle" key={value}>
           {value}
         </td>
       );
     });
     return (
-      <tr className="border-b" key={item.name}>
+      <tr className="border-b border-gray-300" key={item.name}>
         {renderedItem}
       </tr>
     );
@@ -24,13 +27,19 @@ function Table({ data }: TableProp) {
 
   const keys = Object.keys(data[0]);
   const renderedKeys = keys.map((key) => {
-    return <th key={key}>{key}</th>;
+    const capitalizedKey =
+      key.slice(0, 1).toLocaleUpperCase() + key.slice(1).toLocaleLowerCase();
+    return (
+      <th key={key} className="px-6 py-3 text-gray-800">
+        {capitalizedKey}
+      </th>
+    );
   });
 
   return (
-    <table className="table-auto border-spacing-2">
+    <table className="table-auto border-spacing-6">
       <thead>
-        <tr className="border-b-2">{renderedKeys}</tr>
+        <tr className="border-b-2 border-black">{renderedKeys}</tr>
       </thead>
       <tbody>{renderedData}</tbody>
     </table>
--- src/pages/TablePage.tsx
+++ src/pages/TablePage.tsx
@@ -2,10 +2,10 @@ import Table from "../components/Table";
 
 function TablePage() {
   const tableContent = [
-    { name: "Orange", color: "bg-orange-500", quantity: 5 },
-    { name: "Apple", color: "bg-red-500", quantity: 3 },
-    { name: "Banana", color: "bg-yellow-500", quantity: 12 },
-    { name: "Lime", color: "bg-green-500", quantity: 4 },
+    { name: "Orange", color: "bg-orange-400", quantity: 5 },
+    { name: "Apple", color: "bg-red-700", quantity: 3 },
+    { name: "Banana", color: "bg-yellow-300", quantity: 12 },
+    { name: "Lime", color: "bg-green-400", quantity: 4 },
   ];
 
   return (
