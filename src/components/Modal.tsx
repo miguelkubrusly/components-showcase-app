2745427049d27fcab86a6d7c4d18c866719a4989
Miguel Kubrusly <125589621+miguelkubrusly@users.noreply.github.com>
2024-10-08 12:09:24 -0300

    feat(modal): add max and min height and width

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -16,10 +16,10 @@ function Modal({ children, onClose, actionBar }: ModalProp) {
         onClick={onClose}
         className="fixed inset-0 bg-gray-300 opacity-80"
       ></div>
-      <div className="fixed inset-40 p-10 bg-white border-2 border-solid border-gray-400 border-le">
+      <div className="fixed inset-56 p-8 bg-white border-2 border-solid border-gray-400 border-le min-w-[300px] min-h-[200px] max-w-[400px] max-h-[350px]">
         <div className="flex flex-col justify-between h-full">
           {children}
-          <div className="flex justify-end">{actionBar}</div>
+          <div className="flex justify-end mt-6 p-2">{actionBar}</div>
         </div>{" "}
       </div>
     </div>,
