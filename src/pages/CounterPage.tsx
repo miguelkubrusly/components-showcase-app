da15d5e808d7f82f007667fdaad7d7b221bc8fee
Miguel Kubrusly <125589621+miguelkubrusly@users.noreply.github.com>
2024-10-13 15:23:25 -0300

    feat(counter): create page for both counters

--- src/pages/CounterPage.tsx
+++ src/pages/CounterPage.tsx
@@ -1,7 +1,37 @@
+import { useState } from "react";
 import Counter from "../components/Counter";
+import Dropdown from "../components/Dropdown";
+import ReducerCounter from "../components/ReducerCounter";
 
 function CounterPage() {
-  return <Counter />;
+  const [currentCounter, setNewCounter] = useState<string | null>(null);
+
+  const handleChoice = (newCounter: string) => {
+    setNewCounter(newCounter);
+  };
+
+  const options = [
+    { value: "CounterOne", label: "Counter One" },
+    { value: "CounterTwo", label: "Counter Two" },
+  ];
+
+  let counterSelect;
+  if (currentCounter === options[0].label) {
+    counterSelect = <Counter />;
+  } else if (currentCounter === options[1].label) {
+    counterSelect = <ReducerCounter />;
+  }
+
+  return (
+    <div>
+      <Dropdown
+        options={options}
+        value={currentCounter}
+        onChange={handleChoice}
+      />
+      <div className="mt-3">{counterSelect}</div>
+    </div>
+  );
 }
 
 export default CounterPage;
