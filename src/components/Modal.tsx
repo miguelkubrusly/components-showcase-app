7b934406cca36d11c85b32028a8cbf711d968ab8
Miguel Kubrusly <125589621+miguelkubrusly@users.noreply.github.com>
2024-10-12 22:50:46 -0300

    fix(modal): adjust css to stay always on middle

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -16,11 +16,14 @@ function Modal({ children, onClose, actionBar }: ModalProp) {
         onClick={onClose}
         className="fixed inset-0 bg-gray-300 opacity-80"
       ></div>
-      <div className="fixed inset-56 p-8 bg-white border-2 border-solid border-gray-400 border-le min-w-[300px] min-h-[200px] max-w-[400px] max-h-[350px]">
-        <div className="flex flex-col justify-between h-full">
-          {children}
-          <div className="flex justify-end mt-6 p-2">{actionBar}</div>
-        </div>{" "}
+
+      <div className="fixed inset-0 flex items-center justify-center p-4">
+        <div className="bg-white border-2 border-solid border-gray-400 min-w-[300px] min-h-[200px] max-w-[400px] max-h-[350px] p-8 rounded shadow-lg">
+          <div className="flex flex-col justify-between h-full">
+            {children}
+            <div className="flex justify-end mt-6 p-2">{actionBar}</div>
+          </div>
+        </div>
       </div>
     </div>,
     document.querySelector(".modal-container")!
